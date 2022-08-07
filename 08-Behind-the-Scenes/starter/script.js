b00f1318a5b432431e68ade4ce5095fece5da938
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-08-07 17:04:42 +0530

    thisKeyWord

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,2 +1,34 @@
 'use strict';
-fun;
+console.log(this);
+
+const calacAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calacAge(26);
+
+const calacAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calacAgeArrow(26);
+
+const jonas = {
+  year: 1991,
+  calacAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calacAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calacAge = jonas.calacAge;
+
+matilda.calacAge();
